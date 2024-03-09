d5fb60da3ab1ac7db9483e9ee9dee5def7f84cc7
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 05:31:43 -0500

    Fixed `BatteryVoltageNotification`

--- Sources/Topdon/BatteryVoltageNotification.swift
+++ Sources/Topdon/BatteryVoltageNotification.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-public struct BatteryVoltageNotification: Equatable, Hashable, Sendable {
+public struct BatteryVoltageNotification: Equatable, Hashable, Codable, Sendable {
     
     public let timestamp: UInt16
     
     public let voltage: UInt16
     
     public init?(data: Data) {
-        guard data.count >= 17, Data(data.prefix(10)) == BT20.Notification.batteryVoltagePrefix.data else {
+        guard data.count >= 17 else {
             return nil
         }
         self.timestamp = UInt16(bigEndian: UInt16(bytes: (data[10], data[11])))
