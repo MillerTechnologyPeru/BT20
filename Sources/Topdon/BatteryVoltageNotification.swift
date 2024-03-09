f9427b20a0cf60f663c7c9f0d3a3a150c69ff16f
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 17:33:16 -0500

    Fixed `BatteryVoltageNotification`

--- Sources/Topdon/BatteryVoltageNotification.swift
+++ Sources/Topdon/BatteryVoltageNotification.swift
@@ -9,15 +9,9 @@ import Foundation
 
 public struct BatteryVoltageNotification: Equatable, Hashable, Codable, Sendable {
     
-    public let timestamp: UInt16
+    public static var opcode: TopdonSerialMessageOpcode { .batteryVoltageNotification }
+    
+    public let timestamp: UInt32
     
     public let voltage: UInt16
-    
-    public init?(data: Data) {
-        guard data.count >= 17 else {
-            return nil
-        }
-        self.timestamp = UInt16(bigEndian: UInt16(bytes: (data[10], data[11])))
-        self.voltage = UInt16(bigEndian: UInt16(bytes: (data[12], data[13])))
-    }
 }
