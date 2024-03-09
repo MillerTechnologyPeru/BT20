37dd6a6be7bce72031c3b02bd8434ce099f757e3
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 05:29:00 -0500

    Updated unit tests

--- Tests/TopdonTests/TopdonTests.swift
+++ Tests/TopdonTests/TopdonTests.swift
@@ -62,15 +62,30 @@ final class TopdonTests: XCTestCase {
     
     func testBT20VoltageLevel() throws {
         
-        let data = Data([0x55, 0xAA, 0x00, 0x0F, 0xFF, 0xF0, 0xDD, 0x03, 0x65, 0xE8, 0x32, 0xAC, 0x30, 0xE6, 0x00, 0x00, 0x1B])
-        
-        guard let notification = BT20.BatteryVoltageNotification(data: data) else {
-            XCTFail()
-            return
+        do {
+            let data = Data([0x55, 0xAA, 0x00, 0x0F, 0xFF, 0xF0, 0xDD, 0x03, 0x65, 0xE8, 0x32, 0xAC, 0x30, 0xE6, 0x00, 0x00, 0x1B])
+            
+            guard let notification = BatteryVoltageNotification(data: data) else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(notification.timestamp, 0x32AC)
+            XCTAssertEqual(notification.voltage, 12518)
+            XCTAssertEqual(Float(notification.voltage) / 1000, 12.518)
         }
         
-        XCTAssertEqual(notification.timestamp, 0x32AC)
-        XCTAssertEqual(notification.voltage, 12518)
-        XCTAssertEqual(Float(notification.voltage) / 1000, 12.518)
+        do {
+            let data = Data([0x55, 0xAA, 0x00, 0x0F, 0xFF, 0xF0, 0xDD, 0x03, 0x65, 0xEC, 0x36, 0xB2, 0x30, 0xE0, 0x00, 0x00, 0x03])
+            
+            guard let notification = BatteryVoltageNotification(data: data) else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(notification.timestamp, 14002)
+            XCTAssertEqual(notification.voltage, 12512)
+            XCTAssertEqual(Float(notification.voltage) / 1000, 12.512)
+        }
     }
 }
