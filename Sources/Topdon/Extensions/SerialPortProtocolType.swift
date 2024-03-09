01e8edebf6e348ca342ea42f1ada3bf599f0429a
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 17:33:41 -0500

    Add `SerialPortProtocolType.topdonBM2`

---
+++ Sources/Topdon/Extensions/SerialPortProtocolType.swift
@@ -0,0 +1,14 @@
+//
+//  SerialPortProtocolType.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/9/24.
+//
+
+import Foundation
+import Telink
+
+public extension SerialPortProtocolType {
+    
+    static var topdonBM2: SerialPortProtocolType { 0x55AA }
+}
