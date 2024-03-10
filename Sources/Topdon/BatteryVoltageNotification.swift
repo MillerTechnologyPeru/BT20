62e8bafb1a3394e0d9da38b6a35c3c723eff3bb4
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-10 16:42:53 -0400

    Updated `BatteryVoltageNotification`

--- Sources/Topdon/BatteryVoltageNotification.swift
+++ Sources/Topdon/BatteryVoltageNotification.swift
@@ -7,11 +7,25 @@
 
 import Foundation
 
-public struct BatteryVoltageNotification: Equatable, Hashable, Codable, Sendable {
+public struct BatteryVoltageNotification: Equatable, Hashable, Codable, Sendable, BT20Message {
     
     public static var opcode: TopdonSerialMessageOpcode { .batteryVoltageNotification }
     
     public let timestamp: UInt32
     
     public let voltage: UInt16
+}
+
+public extension BatteryVoltageNotification {
+    
+    var date: Date { Date(timeIntervalSince1970: TimeInterval(timestamp)) }
+    
+    
+}
+
+extension BatteryVoltageNotification {
+    
+    public var id: UInt32 {
+        timestamp
+    }
 }
