ebc9c1b972acb68e9fe3f61d8a7233993f91df69
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-11 11:40:38 -0400

    Add `CompanyIdentifier.topdon`

---
+++ Sources/Topdon/Extensions/CompanyIdentifier.swift
@@ -0,0 +1,16 @@
+//
+//  CompanyIdentifier.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/11/24.
+//
+
+import Foundation
+import Bluetooth
+
+public extension Bluetooth.CompanyIdentifier {
+    
+    static var topdon: CompanyIdentifier {
+        0x0056
+    }
+}
